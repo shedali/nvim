1c80126e1f0253606c40277d8fcf9b8971621afa
Franz Sittampalam <franz@Franzs-MacBook-Pro.local>
2025-03-13 14:36:08 +0000

    update obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -4,7 +4,24 @@
 -- See the kickstart.nvim README for more information
 return {
   "epwalsh/obsidian.nvim",
-  finder = "telescope",
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = "telescope.nvim",
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    note_mappings = {
+      -- Create a new note from your query.
+      new = "<C-x>",
+      -- Insert a link to the selected note.
+      insert_link = "<C-l>",
+    },
+    tag_mappings = {
+      -- Add tag(s) to current note.
+      tag_note = "<C-x>",
+      -- Insert a tag at the current location.
+      insert_tag = "<C-l>",
+    },
+  },
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = false,
   ft = "markdown",

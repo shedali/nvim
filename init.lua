4a2299a942799c1faa71acea80451b766e98e628
Franz <shedali@gmail.com>
2025-03-19 17:14:16 +0000

    auto eslint

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@
 vim.opt.wrap = true
 vim.opt.linebreak = true
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" }, -- Add more file types if needed
+  command = "EslintFixAll",
+})

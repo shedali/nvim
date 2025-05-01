78ca5ceb33b1b1f61b65a02202a3f5f6c98d8446
Franz <shedali@gmail.com>
2025-05-01 10:40:51 +0100

    fb find buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -71,11 +71,10 @@ end, { desc = "Go to type definition" })
 -- Telescope
 vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 --
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
 vim.keymap.set("n", "<space>fz", ":Telescope zoxide<CR>")
 
 -- open file_browser with the path of the current buffer
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+--vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
 
 -- Alternatively, using lua API
 -- vim.keymap.set("n", "<space>fb", function()

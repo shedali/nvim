34da34d2399e9f9f72c45b3753e24f430c6251e1
Franz Sittampalam <shedali@gmail.com>
2025-05-12 15:34:07 +0100

    adds persistence

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,12 @@
+-- Lua
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+    dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
+    options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" }, -- sessionoptions used for saving
+    pre_save = nil, -- a function to call before saving the session
+    save_empty = false, -- don't save if there are no open file buffers
+  },
+}

1cca2a9e3b807a258dfd0218ce2e128cd4e98203
Franz Sittampalam <shedali@gmail.com>
2025-05-14 23:10:23 +0100

    neogit visually selected stage

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -8,12 +8,25 @@ return {
     'ibhagwan/fzf-lua', -- optional
   },
   config = function()
-    require('neogit').setup {}
+    require('neogit').setup {
+      kind = "split",
+      -- Enable the following setting to allow staging visually selected lines
+      mappings = {
+        -- Modify the status buffer mappings
+        status = {
+          -- Stage visually selected lines
+          ["s"] = "Stage",
+        },
+      },
+    }
     vim.keymap.set('n', '<leader>gs', require('neogit').open, { silent = true, noremap = true })
     vim.keymap.set('n', '<leader>gB', ':Telescope git_branches<CR>', { silent = true, noremap = true })
     vim.keymap.set('n', '<leader>gb', ':Neogit blame<CR>', { silent = true, noremap = true })
     vim.keymap.set('n', '<leader>gc', ':Neogit commit<CR>', { silent = true, noremap = true })
     vim.keymap.set('n', '<leader>gP', ':Neogit push<CR>', { silent = true, noremap = true })
     vim.keymap.set('n', '<leader>gp', ':Neogit pull<CR>', { silent = true, noremap = true })
+
+    -- Add the visual mode mapping for staging selected lines
+    vim.keymap.set('v', '<leader>gs', ':Neogit kind=split action=stage<CR>', { silent = true, noremap = true })
   end,
 }

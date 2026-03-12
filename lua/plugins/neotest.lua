46df4c42ea8b93bc18d30c7581dd796840c0fd9e
Franz Sittampalam <shedali@gmail.com>
2026-03-12 13:36:24 +0000

    neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,25 +1,20 @@
 return {
-  lazy = false,
-  log_level = vim.log.levels.DEBUG,
-  "nvim-neotest/neotest",
-  enabled = true,
-  dependencies = {
-    "marilari88/neotest-vitest",
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    adapters = {
-      ["neotest-vitest"] = {
-        vitestCommand = "bun test", -- Using Bun instead of pnpm (faster!)
-        cwd = vim.fn.getcwd(), -- Ensures it runs in the project root,
-        testPattern = { "*.spec.tsx", "./**/*.spec.tsx", "**/*.spec.tsx" },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
+      opts.adapters["neotest-vitest"] = {
         filter_dir = function(name, rel_path, root)
           return name ~= "node_modules"
         end,
-      },
-    },
+      }
+    end,
   },
 }

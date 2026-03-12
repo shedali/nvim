3b182fc13c937eb9b99488865774643984180ff3
Franz Sittampalam <shedali@gmail.com>
2026-03-12 16:33:30 +0000

    chore: remap leader

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Override LazyVim's default leader (space) with semicolon
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ";"
+
 -- Disable conceal globally to ensure code blocks are visible
 vim.opt.conceallevel = 0
 vim.opt.concealcursor = ""

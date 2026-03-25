002821607decd95cc513067b1c93be951064a938
Franz Sittampalam <shedali@gmail.com>
2026-03-25 13:48:58 +0000

    show builtins

--- lua/plugins/octo.lua
+++ lua/plugins/octo.lua
@@ -48,7 +48,7 @@ return {
       end
       return entries
     end)(),
-    enable_builtin = false, -- shows a list of builtin actions when no action is provided
+    enable_builtin = true, -- shows a list of builtin actions when no action is provided
     default_remote = { "upstream", "origin" }, -- order to try remotes
     default_merge_method = "merge", -- default merge method which should be used for both `Octo pr merge` and merging from picker, could be `merge`, `rebase` or `squash`
     default_delete_branch = false, -- whether to delete branch when merging pull request with either `Octo pr merge` or from picker (can be overridden with `delete`/`nodelete` argument to `Octo pr merge`)

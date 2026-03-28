a829e3576238b3fb55c5063a1b4184eda5b98491
Franz Sittampalam <shedali@gmail.com>
2026-03-28 14:34:48 +0000

    update oil keybinding

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   'stevearc/oil.nvim',
   enabled = true,
   opts = {},
+  keys = {
+    { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+  },
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons', lazy = false },
 }
